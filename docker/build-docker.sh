5d88406c1ec3f58bae846c02700b0a8890a5aea7
James Fleming <james@electronic-quill.net>
2022-09-23 19:39:53 +0200

    Tweak the Docker stuff so it works

--- docker/build-docker.sh
+++ docker/build-docker.sh
@@ -1,4 +1,4 @@
 VERSION='0.0.1'
-cp ../../schemas/bellingcatcat.json ./
+cp ../schemas/bellingcatcat.json ./
 docker build . -t equill/bellingcatcat:$VERSION -t equill/bellingcatcat:latest
 rm bellingcatcat.json
